9040d39fae405b72bcb013cc19e60ada9edbe8cc
Xelven001 <145933649+Xelven001@users.noreply.github.com>
2024-01-31 22:26:08 -0800

    Update

--- app2.py
+++ app2.py
@@ -12,7 +12,7 @@ def get_data2():
     connection = sqlite3.connect('spotify_data.db')
     cursor = connection.cursor()
 
-    selected_genre = request.args.get('genre')
+    selected_genre = request.args.get('genre2')
 
     if selected_genre:
         query = f"SELECT genre, year, count(song) as 'Total Songs', avg(popularity) as 'Average Popularity', avg(danceability) as 'Average Danceability', avg(duration_ms)/1000 as 'Average Duration(Seconds)', avg(energy) as 'Average Energy', avg(key) as 'Average Key', avg(loudness) as 'Average Loudness', avg(mode) as 'Average Mode', avg(speechiness) as 'Average Speechiness', avg(acousticness) as 'Average Acousticness', avg(instrumentalness) as 'Average Instrumentalness', avg(liveness) as 'Average Liveness', avg(valence) as 'Average Valence', avg(tempo) as 'Average Tempo' FROM song_metrics WHERE genre = ? and year BETWEEN '1999' and '2019' AND genre IN ('Hip-Hop','Electronic','Rock','Pop')  GROUP BY genre,year ORDER BY count(song) DESC"
